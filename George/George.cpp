8f4a1fb9ccc07b4bd3f60f6e1d7dddb32039d3f8
Chippogratum <vkajuk1@gmail.com>
2018-03-01 12:55:00 +0200

    1) задача выполнена: программа по выводу информации о фигуре, созданной из выбранного источника, на консоль написана и отредактирована 2) создана функция для получения необходимого источника мешочка (GetPouchSource()) 3) скелет программы в main состоит из четврех пунктов: 1 - установка источника мешочка 2 - получение мешочка из источника 3 - создание фигуры из мешочка 4 - вывод информации о фигуре 4) можно дополнить варианты вывода информации о фигуре (файл, печать, сеть)

--- George/George.cpp
+++ George/George.cpp
@@ -15,34 +15,24 @@ using namespace std;
 
 void SavePouchToFile(CommonPouch);
 void DataOutput(Figure2D*);
+IPouchSource* GetPouchSource();
 
 int main()
 {
-	//IPouchSource* interface; // как задействовать полиморфизм для выбора нужного интерфейса? ДОЛЖЕН ПРИНИМАТЬСЯ?
-	
-	ConsolePouchSource interface;
-	//ConsolePouchSource consoleInterface; // перемеиновать
-	CommonPouch pouch; // создание мешочка 
-	Figure2D* figure; // создание фигуры 
-
-	pouch = interface.GetPouch(); // получение мешочка из файла (ИНТЕРФЕЙС)
-	SavePouchToFile(pouch); // запись мешочка в файл
-	figure = FigureFactory::CreateFigure(pouch); // определение фигуры 
-	DataOutput(figure); // вывод инфо фигуры созданной с мешочка из файла
-
-	//for (int count = 0; count < 3; count++)
-	//{
-	//	ConsolePouchSource interface;
-	//	//FilePouchSource interface;
-	//	try
-	//	{
-	//		DataOutput(FigureFactory::CreateFigure(interface.GetPouch()));
-	//	}
-	//	catch (exception error)
-	//	{
-	//		cout << error.what() << endl << endl;
-	//	}
-	//}
+	for (int count = 0; count < 3; count++)
+	{
+		try
+		{
+			IPouchSource* PouchSourse = GetPouchSource(); // 1) установка источника мешочка
+			CommonPouch pouch = PouchSourse->GetPouch(); // 2) получение мешочка из источника
+			Figure2D* figure = FigureFactory::CreateFigure(pouch); // 3) создание фигуры из мешочка
+			DataOutput(figure); // 4) вывод информации о фигуре
+		}
+		catch (exception error)
+		{
+			cout << error.what() << endl << endl;
+		}
+	}
 		
     return 0;
 }
@@ -52,14 +42,28 @@ void DataOutput(Figure2D* figure)
 	cout << figure->ToString() << endl;
 }
 
+IPouchSource* GetPouchSource()
+{
+	int source;
+	cout << "Console - 1\tFile - 2" << endl << "Select the source of the pouch: ";
+	cin >> source;
+	cout << endl;
+	if (source==1)
+		return new ConsolePouchSource;
+	if (source == 2)
+		return new FilePouchSource;
+	throw exception("Wrong choice!");
+}
+
 void SavePouchToFile(CommonPouch pouch) // отправка мешочка в файл
 {
-	//cout << "Enter the name of the source file: "; // определение имени файла-источника с консоли
-	//cin >> fileNameOut; //
-	ofstream file;
-	string fileName = "Pouch.txt"; // имя файла, на который записывается мешочек
+	string fileName;
+	cout << "Enter the filename for the data of the pouch: "; // определение имени файла-источника с консоли
+	cin >> fileName;
+	cout << endl;
 
+	ofstream file;
 	file.open(fileName, std::ofstream::out); // содержимое файла будет перезаписано
-	file.write((char*)&pouch, sizeof(CommonPouch)); // запись информации размером с мешочек из полученного мешочка в файл
+	file.write((char*)&pouch, sizeof(CommonPouch)); // запись мешочка в файл
 	file.close(); // закрытие файла
 }
